4612953540a7db06693a3aca78c1b3580c452715
Keval Vora <41047220+kvora125@users.noreply.github.com>
2023-11-15 15:34:01 +0530

    comented unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { getSession } from 'next-auth/react';
 
 import Navbar from '@/components/Navbar';
 import Billboard from '@/components/Billboard';
-import MovieList from '@/components/MovieList';
+// import MovieList from '@/components/MovieList';
 import InfoModal from '@/components/InfoModal';
-import useMovieList from '@/hooks/useMovieList';
-import useFavorites from '@/hooks/useFavorites';
+// import useMovieList from '@/hooks/useMovieList';
+// import useFavorites from '@/hooks/useFavorites';
 import useInfoModalStore from '@/hooks/useInfoModalStore';
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -28,8 +28,8 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Home = () => {
-  const { data: movies = [] } = useMovieList();
-  const { data: favorites = [] } = useFavorites();
+  // const { data: movies = [] } = useMovieList();
+  // const { data: favorites = [] } = useFavorites();
   const {isOpen, closeModal} = useInfoModalStore();
 
   return (
