93e952c5a906f623ec280b374efeccc3af6ffeba
Keval Vora <41047220+kvora125@users.noreply.github.com>
2023-11-17 22:45:20 +0530

    fixed netflix logo size

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -84,7 +84,7 @@ const Auth = () => {
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <Link href="/"><Image height={200} width={200} src="/images/logo.png" className="h-12" alt="Logo" /></Link>
+          <Link href="/"><Image height={100} width={200} src="/images/logo.png" className="h-12" alt="Logo" /></Link>
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
