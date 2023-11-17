64d33211975a1df40fb0ea8731bad318c7e50975
Keval Vora <41047220+kvora125@users.noreply.github.com>
2023-11-17 21:57:07 +0530

    added font-awesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,10 @@ import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app'
 
 import '../styles/globals.css';
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
 
+config.autoAddCss = false;
 export default function App({ 
   Component, 
   pageProps: {
